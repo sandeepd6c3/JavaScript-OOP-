0def71c532afe52c0b4efef4a5b567ddd0c1edd5
sandeepd6c3 <sandeep977244@gmail.com>
2026-03-09 18:37:55 +0530

    Toaster Creation

--- script.js
+++ script.js
@@ -52,29 +52,62 @@
 // fun();
 
 
-function createtoaster(config) {
 
+
+
+// function createtoaster(config) {
+
+//     return function (str) {
+//         let div = document.createElement("div");
+//         div.textContent = str;
+//         div.className =
+//             "inline-block bg-gray-800 text-white px-6 py-3 rounded shadow-lg pointer-events-none transition-opacity duration-300";
+//         document.querySelector(".parent").appendChild(div);
+//         setTimeout(() => {
+//             document.querySelector(".parent").removeChild(div);
+//         }, config.duration * 1000);
+//     };
+// }
+// let toaster = createtoaster({
+//     positionX: "right",
+//     positionY: "top",
+//     theame: "dark",
+//     duration: 3,
+// });
+
+// toaster("Download Completed!");
+
+
+// setTimeout(() => {
+//     toaster("File Deleted!");
+// },2000);
+
+
+
+function createToaster(config) {
     return function (str) {
-        let div = document.createElement("div");
-        div.textContent = str;
-        div.className =
-            "inline-block bg-gray-800 text-white px-6 py-3 rounded shadow-lg pointer-events-none transition-opacity duration-300";
-        document.querySelector(".parent").appendChild(div);
-        setTimeout(() => {
-            document.querySelector(".parent").removeChild(div);
-        }, config.duration * 1000);
+
+       let div= document.createElement("div");
+       div.textContent=str;
+       div.className=`inline-block ${config.theme==="dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 rounded shadow-lg pointer-events-none`;
+
+       document.querySelector(".parent").appendChild(div);
+
+       setTimeout(() => {
+        document.querySelector(".parent").removeChild(div);
+       },config.duration*1000)
     };
 }
-let toaster = createtoaster({
+
+let toaster=createToaster({
     positionX: "right",
-    positionY: "top",
-    theame: "dark",
+    positionY: "",
+    theme: "light",
     duration: 3,
 });
 
-toaster("Download Completed!");
- 
+toaster("Download Done");
 
 setTimeout(() => {
-    toaster("File Deleted!");
-},2000);
+    toaster("Harsh accepted your request ")
+}, 2000);
